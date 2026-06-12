91c6d392a71573217472e88a8575b1593acc0688
Sumeet-Y1 <sumeety202@gmail.com>
2026-06-12 15:15:20 +0530

    up-dated terrform and yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,6 +96,13 @@ resource "aws_security_group" "ec2" {
     security_groups = [aws_security_group.alb.id]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
