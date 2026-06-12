c7eadaeadf0903158755ac4f0f0b61890767fad3
Sumeet-Y1 <sumeety202@gmail.com>
2026-06-12 15:34:44 +0530

    upgrade to v3

--- src/main/java/com/devops/aws_blue_green_deployment/AppController.java
+++ src/main/java/com/devops/aws_blue_green_deployment/AppController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @RequestMapping("/api")
 public class AppController {
 
-    private static final String VERSION = "v2";
+    private static final String VERSION = "v3";
 
     @GetMapping("/hello")
     public Map<String, String> hello() {
