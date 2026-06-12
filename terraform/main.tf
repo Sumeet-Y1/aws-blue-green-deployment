ba53a638517a4ae7c65cd2f2f6312b95be3f4f75
Sumeet-Y1 <sumeety202@gmail.com>
2026-06-12 14:33:02 +0530

    add ssm policy to ec2 role

--- terraform/main.tf
+++ terraform/main.tf
@@ -135,6 +135,11 @@ resource "aws_iam_instance_profile" "ec2" {
   role = aws_iam_role.ec2.name
 }
 
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 # ALB
 resource "aws_lb" "main" {
   name               = "bg-alb"
